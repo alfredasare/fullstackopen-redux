43d28e30ea0dbb1b15b727d50e32207bd7949cc2
alfredasare <alfredasare101@gmail.com>
2021-03-20 20:05:37 +0000

    Refactor new note and notes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import {toggleImportanceOf} from "./reducers/noteReducer";
-import {useSelector, useDispatch} from "react-redux";
 import NewNote from "./components/NewNote";
+import Notes from "./components/Notes";
 
 function App() {
-    const dispatch = useDispatch();
-    const notes = useSelector(state => state);
-
-
-
-    const toggleImportance = id => {
-        dispatch(toggleImportanceOf(id));
-
-        console.log(notes);
-    };
 
     return (
         <div>
             <NewNote />
-            <ul>
-                {
-                    notes.map(note => (
-                        <li key={note.id} onClick={() => toggleImportance(note.id)}>
-                            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-                        </li>
-                    ))
-                }
-            </ul>
+            <Notes />
         </div>
     );
 }
